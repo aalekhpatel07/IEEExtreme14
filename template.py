f0e91a54c5fece9f55d2b99aa59df41aaeec15bb
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-10-23 16:55:12 -0500

    Add the template for solutions.

--- template.py
+++ template.py
@@ -1,77 +1,25 @@
 def solve():
     """
-    Solve the given problem here.
-    :return: Expected output.
+    Solve the problem here.
+    :return: The expected output.
     """
     return 0
 
 
 def driver():
     """
-    Write a driver that reads from system input.
-    :return: Nothing.
+    Make sure this driver returns the result.
+    :return: result - Result of computation.
     """
     # a, b = list(map(int, input().split(' ')))
-    # Collect variables as required.
-
-    print(solve())
-    return
+    result = solve()
+    print(result)
+    return result
 
 
 def main():
-    """
-    Call the driver.
-    :return: Nothing.
-    """
-    driver()
-    return
+    return driver()
 
 
 if __name__ == '__main__':
     main()
-
-# ********************** SOLUTION ENDS **********************
-# ********************** TESTING begins **********************
-
-
-# The code that follows is for testing purposes only.
-# This should not be submitted for judging.
-
-
-def file_driver(contents):
-    """
-    Write a file driver that reads from
-    a list of strings where each string
-    correspond to a line of input.
-
-    :param contents: All lines of the file
-    that has the input data.
-    :return: The calculated output.
-    """
-    result = None
-    for row in contents:
-        # a, b = list(map(int, row.split(' ')))
-        # Collect the variables as required.
-        pass
-
-    result = solve()
-    return result
-
-
-class Problem:
-    """
-    The solution class for testing purposes.
-    """
-    def __init__(self, input_data):
-        """
-        Create an instance with the given data
-        so that the output is populated as
-        required.
-        :param input_data: All lines of the file
-        that has the input data.
-        """
-        self.output = file_driver(input_data)
-        pass
-
-
-# ********************** TESTING ENDS **********************
