9889d1683c70c2a5dbb924affd3135d116e36798
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-10-23 16:55:47 -0500

    Remove Class based testing.

--- solutions/Addition.py
+++ solutions/Addition.py
@@ -4,36 +4,14 @@ def solve(a, b):
 
 def driver():
     a, b = list(map(int, input().split(' ')))
+    result = solve(a, b)
     print(solve(a, b))
-    return
-
-
-def main():
-    driver()
-    return
-
-#
-# if __name__ == '__main__':
-#     main()
-
-# When submitting uncomment above and remove the following!
-
-
-def file_driver(contents):
-    # contents contains lines of data.
-    # If just the first line is required, take the first row.
-    if len(contents) == 0:
-        # No input
-        return
-
-    result = None
-    for row in contents:
-        a, b = list(map(int, row.split(' ')))
-        result = solve(a, b)
     return result
 
 
-class Addition:
-    def __init__(self, input_data):
-        self.output = file_driver(input_data)
-        pass
+def main():
+    return driver()
+
+
+if __name__ == '__main__':
+    main()
